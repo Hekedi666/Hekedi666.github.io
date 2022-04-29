d091771af7e026f4205f0c67ccd85290c5780aa6
Veeti Heikkinen <98155252+Hekedi666@users.noreply.github.com>
2022-04-29 16:55:39 +0300

    Lisättiin javascript tehtävät

---
+++ Tehtäviä/scripts/helloworld.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+    console.log("Viesti konsoliin :D");
+}
+
+function sayHelloTo(name) {
+    alert("Hei " + name + "!");
+    console.log("SayHello-funktio suoritettu");
+}
+

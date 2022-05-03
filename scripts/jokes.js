bc1682d3ed0a854b2bfdcbf7a95d727451e1440c
Veeti Heikkinen <98155252+Hekedi666@users.noreply.github.com>
2022-05-03 16:44:43 +0300

    Add files via upload

--- scripts/jokes.js
+++ scripts/jokes.js
@@ -1,18 +1,43 @@
 
+let laskuri = 0;
+
+$( document ).ready(function() {
+    $('p').text("Onnistuneita hakuja: ");
+    $("#wonk").text("Onnistuneita hakuja: " + laskuri);
+  });
+
 $("#btn").click(function() {
     let nimmi = $("#käyttis").val();
-    let vidu = "https://api.github.com/users/"+nimmi;
+    let vidu = "https://api.github.com/users/" + nimmi;
 
     $.ajax({
         url: vidu,
         type: "GET",
         dataType: "json",
         success: function (response) {
-            $("#account-data").append("<br>" + "<i>" + response.name + "</i>");
+
+            setTimeout(function() {
+                $("#account-data").append("<br>" + "<i>" + response.name + "</i>");
+                $("#account-data").append("<br>" + "<i>" + response.bio + "</i>");
+                $("#account-data").append("<br>" + "<i>" + response.blog + "</i>");
+                laskuri = laskuri + 1;
+                $("#wonk").text("Onnistuneita hakuja: " + laskuri);
+            }, 3000);
         },
         error: function (jqxhr, textStatus, error) {
             console.log("Error: " + textStatus);
             $("#account-data").append("<br>" + "<i>" + "Ei löydy" + "</i>");
         },
     });
+});
+
+$(document).on({
+    ajaxStart: function(){
+        $("body").addClass("loading"); 
+    },
+    ajaxStop: function(){ 
+        setTimeout(function(){
+            $("body").removeClass("loading"); 
+        }, 3000);
+    }    
 });
